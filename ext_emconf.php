4365932abac1145669338ce7a22de0cf88ef0702
Denis Doerner <d.doerner@ecentral.de>
2025-09-16 14:27:26 +0200

    Edit ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.99.99',
+            'typo3' => '12.0.0-13.99.99',
         ],
         'conflicts' => [
         ],
